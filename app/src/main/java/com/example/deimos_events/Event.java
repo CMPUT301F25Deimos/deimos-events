50bf9e7e9ba531a85e2b97ced23c41dc1087cc9f
Muhammad Khawaja <96965476+riazkhawaja@users.noreply.github.com>
2025-11-04 21:50:57 -0700

    Added the main classes we need, constructors, getters and setters

--- app/src/main/java/com/example/deimos_events/Event.java
+++ app/src/main/java/com/example/deimos_events/Event.java
@@ -3,11 +3,10 @@ package com.example.deimos_events;
 import java.util.Date;
 
 /**
- * A class for events
+ * A class for events. This also contains methods for getting data related to registrations and users.
  */
 public class Event {
     String id;
-    String[] waitingListParticipantIds;
     String title;
     String posterId;
     String description;
@@ -16,9 +15,8 @@ public class Event {
     Boolean recordLocation;
     String qrCodeId;
 
-    public Event(String id, String[] waitingListParticipantIds, String title, String posterId, String description, Date registrationDeadline, Number participantCap, Boolean recordLocation) {
+    public Event(String id, String title, String posterId, String description, Date registrationDeadline, Number participantCap, Boolean recordLocation) {
         this.id = id;
-        this.waitingListParticipantIds = waitingListParticipantIds;
         this.title = title;
         this.posterId = posterId;
         this.description = description;
@@ -30,18 +28,66 @@ public class Event {
     public String getTitle() {
         return title;
     }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getPosterId() {
+        return posterId;
+    }
+
+    public void setPosterId(String posterId) {
+        this.posterId = posterId;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Date getRegistrationDeadline() {
+        return registrationDeadline;
+    }
+
+    public void setRegistrationDeadline(Date registrationDeadline) {
+        this.registrationDeadline = registrationDeadline;
+    }
+
+    public Number getParticipantCap() {
+        return participantCap;
+    }
+
+    public void setParticipantCap(Number participantCap) {
+        this.participantCap = participantCap;
+    }
+
+    public Boolean getRecordLocation() {
+        return recordLocation;
+    }
+
+    public void setRecordLocation(Boolean recordLocation) {
+        this.recordLocation = recordLocation;
+    }
+
+    public String getQrCodeId() {
+        return qrCodeId;
+    }
+
+    public void setQrCodeId(String qrCodeId) {
+        this.qrCodeId = qrCodeId;
+    }
+
     public String getId() {
         return id;
     }
 
-    public String getDescription(){return description;}
-    
-
-    public Number getNumberOfEntrants() {
-        return waitingListParticipantIds.length;
+    public void setId(String id) {
+        this.id = id;
     }
-
-
 }
 
 
